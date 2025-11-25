ff08e0633ac23247dccede5b39110ee17dae6b57
umittadelen <72702832+umittadelen@users.noreply.github.com>
2025-11-25 19:15:45 +0100

    service worker added

---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = "mazegen-v1";
+
+const FILES_TO_CACHE = [
+  "index.html",
+  "gen.html",
+  "scripts/main.js",
+  "scripts/gen.js",
+  "icons/icon-60.png",
+  "icons/icon-76.png",
+  "icons/icon-120.png",
+  "icons/icon-152.png",
+  "icons/icon-180.png"
+];
+
+// Install: cache everything
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate: remove old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: offline first
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      return (
+        res ||
+        fetch(event.request).then((fetchRes) => {
+          return fetchRes;
+        })
+      );
+    })
+  );
+});
